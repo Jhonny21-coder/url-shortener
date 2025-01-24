2ce8ae03965d404552bb1c673594352922cd2573
unknown <shanemarahay@gmail.com>
2025-01-24 16:20:57 +0800

    pushed a project

---
+++ src/hooks/useQuery.js
@@ -0,0 +1,60 @@
+import { useQuery } from "react-query";
+import api from "../api/api";
+
+export const useFetchMyShortUrls = (token, onError) => {
+  return useQuery(
+    "my-shortenurls",
+    async () => {
+      return await api.get("/api/urls/myurls", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    },
+    {
+      select: (data) => {
+        const sortedData = data.data.sort(
+          (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+        );
+        return sortedData;
+      },
+      onError,
+      staleTime: 5000,
+    }
+  );
+};
+
+export const useFetchTotalClicks = (token, onError) => {
+  const currentYear = new Date().getFullYear();
+  const startDate = `${currentYear}-01-01`;
+  const endDate = `${currentYear}-12-31`;
+
+  return useQuery(
+    "url-totalclick",
+    async () => {
+      return await api.get(
+        `/api/urls/totalClicks?startDate=${startDate}&endDate=${endDate}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+    },
+    {
+      select: (data) => {
+        const convertToArray = Object.keys(data.data).map((key) => ({
+          clickDate: key,
+          count: data.data[key],
+        }));
+        return convertToArray;
+      },
+      onError,
+      staleTime: 5000,
+    }
+  );
+};
